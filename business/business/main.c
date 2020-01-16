866e753c4b1b0872cd9876aead3bcebf2e0ab76d
OpSide <43568538+OpSide@users.noreply.github.com>
2020-01-16 18:08:19 +0200

    update the menu and syntax

--- business/business/main.c
+++ business/business/main.c
@@ -4,6 +4,8 @@
 //#include "doublyLinkedList.h"
 #include "doubly_list_users.h"
 #include "doubly_list_tickets.h"
+#include <conio.h>
+#include "conio.h"
 
 char* fp_path;
 
@@ -68,7 +70,7 @@ void main() {
 	//DeleteTicket(TICKETS_PATH, ticketsList);
 	while (LogIn(usersList, currentUser) != 1);
 	//AddUser(USERS_PATH, lst);
-	printf("Welcome to CAR SYSTEM! %s!\n", currentUser->fullName);
+	printf("%s, Welcome to Garage System!\n", currentUser->fullName);
 	//getDateTime(&day, &month, &year, &hours, &mins);
 	//printf("Current computer time: %02d/%02d/%d, %02d:%02d\n", day, month, year, hours, mins);
 		/* Display Menu */
@@ -117,11 +119,11 @@ int LogIn(listUsers *lst, user* currentUser)
 	const ARRAY_SIZE = 15;
 	tempNode = lst->head;
 
-
-	printf("Choose username: ");
+	printf("Log-in to your account:");
+	printf("Enter Username: ");
 	scanf("%s", &username);
 
-	printf("Choose password: ");
+	printf("Enter password: ");
 	scanf("%s", &password);
 
 
@@ -152,22 +154,23 @@ void menu(user* current, listUsers* tickets)
 	while (1) {
 		// MENU FOR client 
 		printf("\n //////////CAR SYSTEM MENU////////////\n");
-		printf("\n *  [1] Search car ticket	           *\n");
-		printf("\n *  [2] Add car ticket               *\n");
+		printf("\n *	[1] Search car ticket			*\n");
+		printf("\n *	[2] Add car ticket				*\n");
 		if (current->level>1){
-		printf("\n *  [3] Edit car ticket		       *\n");
-		printf("\n *  [4] Delete car ticket			   *\n");
+		printf("\n *	[3] Edit car ticket				*\n");
+		printf("\n *	[4] Delete car ticket			*\n");
 		}
 		if (current->level==3){
-		printf("\n *  [5] Admin area			       *\n");
+		printf("\n *	[5] Admin area					*\n");
 		}
-		printf("\n Please select your action [1-5]. to exit press [0]:");
+		printf("\n Please select your action [1-5]. to exit press [0]");
 		scanf("%d", &menu_option);
 
 		switch (menu_option) {
 
 		//Search cars (Menu option)
 		case 1: {
+			printf("\n========================================================================================\n");
 			printf("choose searching by CAR NUMBER [1] or STATUS [2] or BOTH [3] or DATE [4]. to Exit press 0.\n");
 			printf("enter your option: ");
 			scanf("%d", &action);
@@ -176,28 +179,28 @@ void menu(user* current, listUsers* tickets)
 			{
 				// Go back to main menu
 				case 0: {
-					printf("Bye Bye...\n");
+					printf("*MSG: Exit to menu.\n");
 					break;
 				}
 				// Search CAR
 				case 1: {
-					printf("Enter CAR number: ");
+					printf("*MSG: You selected option number [1]\n");
 					break;
 				}
 				// Search  STATUS
 				case 2: {
-					printf("Enter STATUS: [1] sent to the customer. [0] In the garage: ");
+					printf("*MSG: You selected option number [2]\n");
+					printf("Enter STATUS: [1] present sent to the customer. [0] present In the garage:");
 					break;
 				}
 				// Search by CAR NUMBER & STATUS
 				case 3: {
-					printf("Enter car number: ");
-					printf("Enter car status:");
+					printf("*MSG: You selected option number [3]\n");
 					break;
 				}
 				// Search DATE
 				case 4: {
-					printf("Enter the date you want to search for: ");
+					printf("*MSG: You selcted option number [4]\n");
 					break;
 				}
 				// Invalid input from client
@@ -212,13 +215,14 @@ void menu(user* current, listUsers* tickets)
 
 		//Add car ticket (Menu option)
 		case 2: {
-			printf("add new car ticket to the system");
+			printf("\n========================================================================================\n");
 			break;
 		}
 		// Edit Car tickets function (Menu option)
 		case 3: {
 			if (current->level>1){
-				printf("Edit exist TICKET");
+				printf("\n========================================================================================\n");
+				printf("function is here");
 			}
 			else {
 				printf("You dont have PERMISSIONS. Contact with the administrator.\n");
@@ -229,7 +233,8 @@ void menu(user* current, listUsers* tickets)
 		// Delete Ticket (Menu option)
 		case 4: {
 			if (current->level>1){
-				printf("Delte ticket\n");
+				printf("\n========================================================================================\n");
+				printf("Delte ticket function\n");
 				}
 				else {
 				printf("You dont have PERMISSIONS. Contact with the administrator.\n");
@@ -241,52 +246,61 @@ void menu(user* current, listUsers* tickets)
 		//Admin Area ( Menu option)
 		case 5: {
 			if (current->level==3){
+				printf("\n========================================================================================\n");
 				printf("Welcome to Admin area! Be careful with your operations.\n");
-				printf("**************************************************\n");
-				printf("[1] Print logs \n");
-				printf("[2] Print users\n");
-				printf("[3] Print Car Database\n");
-				printf("[4] Change user permissions\n");
-				printf("[5] Delete user from databse\n");
-				printf(" Remmeber: Press 0 to exit\n");
-				printf("**************************************************");
-				printf("**BE CAREFUL**\n");
-				printf("\n");
-				printf("enter your option: ");
+				printf ("\n");
+				printf("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Admin Area^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+				printf("[1]		Print logs														^\n");
+				printf("[2]		Print users														^\n");
+				printf("[3]		Print Car Database												^\n");
+				printf("[4]		Change user permissions											^\n");
+				printf("[5]		Delete user from databse										^\n");
+				printf("To exit to main menu press [0]. Remmber you are in Admin Mode.")
+				printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+				printf("\n \n");
+				printf("Enter your option:");
 				scanf("%d", &action);
 			switch(action){
 				case 0: {
-					printf("Bye Bye...\n");
+					printf("\n========================================================================================\n");
+					printf("*MSG: Exit to menu.\n");
 					break;
 				}
 				//print logs function
 				case 1: {
+					printf("\n========================================================================================\n");
 					printf("Printing logs... Please wait\n");
 					break;
 				}
 				//print users function
 				case 2: {
+					printf("\n========================================================================================\n");
 					printf("Printing users... Please wait\n");
 					break;
 				}
 				//print car database function
 				case 3: {
+					printf("\n========================================================================================\n");
 					printf("Printing car database... Please wait\n");
 					break;
 				}
 				//change user permission function
 				case 4: {
-					printf("Change user premission\n");
+					printf("\n========================================================================================\n");
+					printf("Change user premission function\n");
 					break;
 				}
 				//delete user function
 				case 5: {
-					printf("Delete user \n");
+					printf("\n========================================================================================\n");
+					printf("Delete user function \n");
 					break;
 				}
 				default: {
-					printf(" Bye Bye...\n");
-					return 1;
+					printf("ERROR: Invalid number. Enter vaild number!\n");
+					printf("Re-Enter option:");
+					scanf("%d", &action);
+					break;
 				}
 			}
 			}
