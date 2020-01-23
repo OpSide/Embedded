017b0f6f9b2a7414c804c90bd9e6df408953c3f7
OpSide <43568538+OpSide@users.noreply.github.com>
2020-01-23 19:56:17 +0200

    my files

--- business/business/temp_raviv.c
+++ business/business/temp_raviv.c
@@ -4,16 +4,17 @@
 #include "print_l.h"
 #include <string.h>
 
+void redColor () {
+  printf("\033[1;31m");
+}
+void WhiteColor () {
+  printf("\033[0m");
+}
+void GreenColor () {
+  printf("\033[0;32m");
+}
 void main (){
-    PrintLogo();
-    char* yes_char[1];
 
-    printf("enter char: ");
-    scanf("%s", &*yes_char);
-if(strcmp(*yes_char, "y") == 0){
-
-      printf ("\n you are OK!");
-    }
-    printf ("\nyou are not GOOD !\n");
-    printf("%s\n", *yes_char);
+red();
+printf("hi");
 }
